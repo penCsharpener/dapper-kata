90c4c23f0de7ffc6439889b1dccc5c3c1b2e104d
penCsharpener <matthiasmueller@protonmail.com>
2019-04-04 23:31:38 +0200

    add MySqlConnectionDetails, refactor DataConnection

--- dapper-kata/DataConnection.cs
+++ dapper-kata/DataConnection.cs
@@ -4,47 +4,12 @@ using System.Text;
 namespace DapperKata {
     public class DataConnection {
 
-        public DataConnection() {
+        public MySqlConnectionDetails ConnectionDetails { get; }
 
+        public DataConnection(MySqlConnectionDetails connectionDetails) {
+            ConnectionDetails = connectionDetails;
         }
 
-        public DataConnection(string server, string user, string password, ushort port, string database) {
-            Server = server;
-            User = user;
-            Password = password;
-            Port = port;
-            Database = database;
-        }
 
-        public string Server { get; set; } = "localhost";
-        public string User { get; set; } = "root";
-        public string Password { get; set; }
-        public ushort Port { get; set; } = 3306;
-        public string Database { get; set; }
-        public bool RequireSSL { get; set; }
-        public bool ConvertDateTime { get; set; } = true;
-        public string CharSet { get; set; }
-
-        private string BuildConnectionString() {
-            var sb = new StringBuilder();
-            sb.Append("Server=").Append(Server).Append(";")
-                .Append("Uid=").Append(User).Append(";")
-                .Append("Pwd=").Append(Password).Append(";")
-                .Append("Port=").Append(Port).Append(";");
-            if (!string.IsNullOrEmpty(Database)) {
-                sb.Append("Database=").Append(Database).Append(";");
-            }
-            if (RequireSSL) {
-                sb.Append("SslMode=Required;");
-            }
-            if (ConvertDateTime) {
-                sb.Append("ConvertZeroDateTime=True;");
-            }
-            if (!string.IsNullOrEmpty(CharSet)) {
-                sb.Append("CharSet=").Append(CharSet).Append(";");
-            }
-
-            return sb.ToString();
-        }
     }
 }
---
+++ dapper-kata/MySqlConnectionDetails.cs
@@ -0,0 +1,104 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DapperKata {
+    public class MySqlConnectionDetails {
+
+        public MySqlConnectionDetails(string server, string user, string password, ushort port, string database) {
+            Server = server;
+            User = user;
+            Password = password;
+            Port = port;
+            Database = database;
+        }
+
+        private string _server = "localhost";
+        public string Server {
+            get { return _server; }
+            set { _server = value; refreshConnectionString = true; }
+        }
+
+        private string _user = "root";
+        public string User {
+            get { return _user; }
+            set { _user = value; refreshConnectionString = true; }
+        }
+
+        private string? _password;
+        public string? Password {
+            get { return _password; }
+            set { _password = value; refreshConnectionString = true; }
+        }
+
+        private ushort _port = 3306;
+        public ushort Port {
+            get { return _port; }
+            set { _port = value; refreshConnectionString = true; }
+        }
+
+        private string? _database;
+        public string? Database {
+            get { return _database; }
+            set { _database = value; refreshConnectionString = true; }
+        }
+
+        private bool _requireSSL;
+        public bool RequireSSL {
+            get { return _requireSSL; }
+            set { _requireSSL = value; refreshConnectionString = true; }
+        }
+
+        private bool _convertDateTime = true;
+        public bool ConvertDateTime {
+            get { return _convertDateTime; }
+            set { _convertDateTime = value; refreshConnectionString = true; }
+        }
+
+        private string? _charSet;
+        public string? CharSet {
+            get { return _charSet; }
+            set { _charSet = value; refreshConnectionString = true; }
+        }
+
+        private bool refreshConnectionString;
+
+        private string? _ConnectionString;
+        [JsonIgnore]
+        public string? ConnectionString {
+            get {
+                if (string.IsNullOrEmpty(_ConnectionString) || refreshConnectionString) {
+                    _ConnectionString = BuildConnectionString();
+                    refreshConnectionString = false;
+                }
+                return _ConnectionString;
+            }
+        }
+
+        [JsonIgnore]
+        public bool ConnectionStringInvalid => string.IsNullOrEmpty(Server) || string.IsNullOrEmpty(User);
+
+        private string BuildConnectionString() {
+            var sb = new StringBuilder();
+            sb.Append("Server=").Append(Server).Append(";")
+                .Append("Uid=").Append(User).Append(";")
+                .Append("Pwd=").Append(Password).Append(";")
+                .Append("Port=").Append(Port).Append(";");
+            if (!string.IsNullOrEmpty(Database)) {
+                sb.Append("Database=").Append(Database).Append(";");
+            }
+            if (RequireSSL) {
+                sb.Append("SslMode=Required;");
+            }
+            if (ConvertDateTime) {
+                sb.Append("ConvertZeroDateTime=True;");
+            }
+            if (!string.IsNullOrEmpty(CharSet)) {
+                sb.Append("CharSet=").Append(CharSet).Append(";");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
